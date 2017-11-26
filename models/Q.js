f10def1cb7da402da36ec3338129a084fa7b6ca3
xpfoursquared <xinpengxp@live.com>
2017-11-26 09:22:32 +0000

    Corrected Q logic and implemented method to present data

--- models/Q.js
+++ models/Q.js
@@ -3,6 +3,27 @@ function Q(io) {
   this.Q = [];
   this.QLength = 0;
 
+  
+  this.getLength = function() {
+    var sum = 0;
+    this.Q.forEach(function(element) {
+      sum += element.groupsize;
+    });s
+ 
+    return sum;
+  };
+
+
+  this.formatToday = function(d) {
+    var hr = d.getHours();
+    var min = d.getMinutes();
+    if (min < 10) {
+        min = "0" + min;
+    }
+    return hr+":"+min;
+  }
+
+
   this.enQ = function(user, phonenumber, groupsize) {
     var entry = {
       user: user,
@@ -11,18 +32,33 @@ function Q(io) {
       time: new Date(),
     }
     this.Q.push(entry);
-    this.QLength = this.Q.length;
+    this.QLength = this.getLength();
     io.sockets.emit("enQ", this.QLength);
     return this.QLength;
   };
 
+
   this.deQ = function() {
-    var removed = this.Q.pop();
-    this.QLength = this.Q.length;
+    this.Q.pop();
+    tthis.QLength = this.getLength();
     io.sockets.emit("deQ", this.QLength);
     return this.QLength;
   };
 
+
+  this.trend = function() {
+    data_points = [];
+    this.Q.forEach(function(element){
+      var entry = {
+        time: this.formatToday(element.time),
+        data: element.groupsize,
+      }
+
+      data_points.push(entry);
+    })
+
+    return data_points;
+  };
 }
 
 exports.Q = Q;
