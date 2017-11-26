b43b63e5528a6455b0bf79900c8a60da17ff66c6
xpfoursquared <xinpengxp@live.com>
2017-11-26 11:19:45 +0000

    again

--- models/generate_fake_data.js
+++ models/generate_fake_data.js
@@ -9,7 +9,7 @@ var randnorm = require("randgen")
 exports.fakeToday = function (io) {
   boothnames = [];
   for (var i=0; i<200; i++) {
-    boothnames.push(generate.spaced);
+    boothnames.push(generate().spaced);
   }
 
   boothnames.forEach(function(name){
@@ -18,7 +18,7 @@ exports.fakeToday = function (io) {
 
   boothnames.forEach(function(fakename) {
     boothcontroller.enQ(fakename,
-      generate.dashed,
+      generate().dashed,
       randomstring.generate({charset: "0123456789", length: 11}),
       Math.ceil(randnorm.rnorm()*5));
   });
